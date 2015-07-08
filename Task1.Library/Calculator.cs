8d5da2f9b3215a4d221920ea4daba618edb77a5d
helenakolodko <helena.kolodko@gmail.com>
2015-07-09 00:22:55 +0300

    Initial implementaion of task 1 algorithm

---
+++ Task1.Library/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Library
+{
+    public static class Calculator
+    {
+        public static double Root(double x, double n, double precision)
+        {
+            double result = x;
+            double next = x / n;
+            while (Math.Abs(next - result) > precision)
+            {
+                result = next;
+                next = (1 / n) * ((n - 1) * result + x / Math.Pow(result, n - 1));
+            }
+            return next;
+        }
+    }
+}
